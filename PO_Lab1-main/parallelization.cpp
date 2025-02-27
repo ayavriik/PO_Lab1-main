b67fff2fdd2007fa1578ee7c611c1db0226cfad5
DESKTOP-LOU0FN9 <yarusha4true@gmail.com>
2025-02-27 15:58:00 +0200

    cmt 10

--- PO_Lab1-main/parallelization.cpp
+++ PO_Lab1-main/parallelization.cpp
@@ -4,12 +4,18 @@ const int n = 10000;
 const int m = 10000;
 
 int main() {
-    int** matrix = new int*[n]; 
+    int** matrix = new int*[n];
     for (int i = 0; i < n; ++i) {
-        matrix[i] = new int[m]; 
+        matrix[i] = new int[m];
     }
 
-    std::cout << "Memory allocated for matrix." << std::endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            matrix[i][j] = j + 1;
+        }
+    }
+
+    std::cout << "Matrix initialized." << std::endl;
 
     return 0;
 }
