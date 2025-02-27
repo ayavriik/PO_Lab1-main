e0ddd9518f05985b3735714067b7d74b61c3a355
DESKTOP-LOU0FN9 <yarusha4true@gmail.com>
2025-02-27 13:43:18 +0200

    cmt 2

--- PO_Lab1-main/main.cpp
+++ PO_Lab1-main/main.cpp
@@ -4,6 +4,12 @@ const int n = 10000;
 const int m = 10000;
 
 int main() {
-    std::cout << "Matrix size: " << n << "x" << m << std::endl;
+    int** matrix = new int*[n]; // Виділяємо пам'ять під покажчики на рядки
+    for (int i = 0; i < n; ++i) {
+        matrix[i] = new int[m]; // Виділяємо пам'ять для кожного рядка
+    }
+
+    std::cout << "Memory allocated for matrix." << std::endl;
+
     return 0;
 }
