d23c1b974ae7e2630edeb4309d90663a4e0278c6
DESKTOP-LOU0FN9 <yarusha4true@gmail.com>
2025-02-27 16:20:07 +0200

    cmt 11

--- PO_Lab1-main/parallelization.cpp
+++ PO_Lab1-main/parallelization.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
+#include <thread>
 
 const int n = 10000;
 const int m = 10000;
+const int numThreads = 6;
 
 int main() {
     int** matrix = new int*[n];
     for (int i = 0; i < n; ++i) {
         matrix[i] = new int[m];
-    }
-
-    for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             matrix[i][j] = j + 1;
         }
     }
 
-    std::cout << "Matrix initialized." << std::endl;
+    long* rowSums = new long[n](); 
+
+    std::thread* threads = new std::thread[numThreads]; 
+    int chunkSize = n / numThreads;
+
+    for (int i = 0; i < numThreads; ++i) {
+        int start = i * chunkSize;
+        int end = (i == numThreads - 1) ? n : (i + 1) * chunkSize;
+
+        threads[i] = std::thread([matrix, &rowSums, start, end]() {
+            for (int i = start; i < end; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    rowSums[i] += matrix[i][j];
+                }
+            }
+        });
+    }
+
+    std::cout << "Threads started." << std::endl;
 
     return 0;
 }
